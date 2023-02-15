66b8584a040d0b72ec9b6daa2df9dee0eaf1f12c
iffalimaturrohmah <109191221+iffalimaturrohmah@users.noreply.github.com>
2023-02-15 15:08:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko  Motor</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <center>
+    <h1>
+        <span style="--i:1;">A</span>
+        <span style="--i:2;">P</span>
+        <span style="--i:3;">L</span>
+        <span style="--i:4;">I</span>
+        <span style="--i:5;">K</span>
+        <span style="--i:6;">A</span>
+        <span style="--i:7;">S</span>
+        <span style="--i:8;">I</span>
+        <h1 contenteditable="true">DVD</h1>
+    <h4><a class="action-btn" href="tampil.php">Lihat Data</a></h4>
+    <h4><a class="action-btn" href="tambah.php" name="tampil_data" value="tampil_data">Tambah Data</a></h4><br>
+    <br>
+    </center>
+</body>
+</html>
